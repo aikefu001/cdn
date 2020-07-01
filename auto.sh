5b881db2b3f478d4bdea8d45d84d45b019a4d2ad
Yang Wu (yangwu3) <yangwu3@cisco.com>
2020-07-01 13:22:49 +0800

    200701_commit

--- auto.sh
+++ auto.sh
@@ -3,7 +3,7 @@
 commit_name="`date +%y%m%d`_commit" 
 echo "====git auto push start..."
 git add .
-git commit -m '$commit_name'
+git commit -m $commit_name
 git push origin master
 echo "====git auto push end..."
 
